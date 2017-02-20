025d501151b57b685a667910e6361357ed50698f
Weiming Zhai <wmzhai@gmail.com>
2017-02-20 20:13:59 +0800

    navigation on successful mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import gql from 'graphql-tag';
 import {graphql} from 'react-apollo';
+import {Link,hashHistory} from 'react-router';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -13,14 +14,15 @@ class SongCreate extends Component {
     event.preventDefault();
     this.props.mutate({
       variables : {
-      title: this.state.title
+        title: this.state.title
       }
-    });
+    }).then(() => hashHistory.push('/') );
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
          <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
